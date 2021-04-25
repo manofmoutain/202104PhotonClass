e676f43e32abfb1be8327ac3d7bd8ee74641a9c1
123 <123>
2021-04-25 12:05:45 +0800

    Call Tim's CallBack.cs

---
+++ Assets/Kujata/Scripts/Main/PhotonCallBackManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Tim;
+
+namespace Kujata.Scripts.Main
+{
+    public class PhotonCallBackManager : MyPhotonRealtimeCallBack
+    {
+        [SerializeField] private BtnManager _btnManager;
+        
+        public override void OnConnected()
+        {
+            base.OnConnected();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            base.OnConnectedToMaster();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            base.OnJoinedLobby();
+            _btnManager.lobbyGO.SetActive(true);
+        }
+    }
+
+}
